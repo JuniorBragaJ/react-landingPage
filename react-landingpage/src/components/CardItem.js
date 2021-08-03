f453eb52d4c3df2b0d70652801e15ff1a3759553
Junior Braga <jpbraga.junior@gmail.com>
2021-08-03 10:55:56 -0300

    melhorando o css dos cards

--- react-landingpage/src/components/CardItem.js
+++ react-landingpage/src/components/CardItem.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Cards.css';
 
 function CardItem(props) {
      return (
@@ -8,10 +9,10 @@ function CardItem(props) {
              <Link className="cards__item__link" to={props.path}>
 
                 <figure className="cards__item_pic-wrap" data-category={props.label}>
-                    <img src={props.src} alt="Services"/>
+                    <img className="cards__item__img"src={props.src} alt="Services"/>
                 </figure>
 
-                <div className="cards__item_info">
+                <div className="cards__item__info">
                     <h5 className="cards__item__text">{props.text}</h5>
                 </div>
 
