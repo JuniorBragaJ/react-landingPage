ba7f8ed79029276d230deba15b2e52fff768ce37
Junior Braga <jpbraga.junior@gmail.com>
2021-08-03 18:53:17 -0300

    css dos cards melhorado

--- react-landingpage/src/components/Cards.js
+++ react-landingpage/src/components/Cards.js
@@ -10,8 +10,34 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
+                        src="images/img-2.jpg"
+                        text="Direito civil"
+                        label="Serviços"
+                        path="/services"
+                        />
+                        <CardItem
                         src="images/img-9.jpg"
-                        text="Trabalhamos com estes tipos de crime"
+                        text="Direito administrativo"
+                        label="Serviços"
+                        path="/services"
+                        />
+                    </ul>
+                    <ul className="cards__items">
+                    <CardItem
+                        src="images/img-1.jpg"
+                        text="Direito ambiental"
+                        label="Serviços"
+                        path="/services"
+                        />
+                        <CardItem
+                        src="images/img-4.jpg"
+                        text="Direito do consumidor"
+                        label="Serviços"
+                        path="/services"
+                        />
+                        <CardItem
+                        src="images/img-5.jpg"
+                        text="Direito contratual"
                         label="Serviços"
                         path="/services"
                         />
